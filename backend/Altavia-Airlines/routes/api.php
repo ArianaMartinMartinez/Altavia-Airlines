5694c4cafde0fefb99f4b12d9277f3be18ee794c
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-03 15:38:05 +0100

    feat: add routes for user index and show

--- backend/Altavia-Airlines/routes/api.php
+++ backend/Altavia-Airlines/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/* Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); */
+
+// USERS
+Route::get('/users', [UserController::class, 'index'])->name('apiHomeUsers');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('apiShowUsers');
