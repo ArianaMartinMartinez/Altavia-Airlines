366985affc45485a4d0dee37177fe01d7c7e4c0c
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-03 23:35:01 +0100

    feat: add routes for index and show for Cities

--- backend/Altavia-Airlines/routes/api.php
+++ backend/Altavia-Airlines/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CityController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,3 +15,7 @@ Route::get('/users/{id}', [UserController::class, 'show'])->name('apiShowUser');
 Route::post('/users', [UserController::class, 'store'])->name('apiStoreUser');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('apiUpdateUser');
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('apiDestroyUser');
+
+// CITIES
+Route::get('/cities', [CityController::class, 'index'])->name('apiHomeCities');
+Route::get('/cities/{id}', [CityController::class, 'show'])->name('apiShowCity');
