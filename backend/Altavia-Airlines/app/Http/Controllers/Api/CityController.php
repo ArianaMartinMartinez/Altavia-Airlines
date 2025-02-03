637be5d56bcdf425d2ce33389b7507fd29477c83
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-03 23:34:44 +0100

    feat: add index and show methods for Cities

---
+++ backend/Altavia-Airlines/app/Http/Controllers/Api/CityController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\City;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $cities = City::all();
+
+        return response()->json($cities, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $city= City::findOrFail($id);
+
+        return response()->json($city, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
