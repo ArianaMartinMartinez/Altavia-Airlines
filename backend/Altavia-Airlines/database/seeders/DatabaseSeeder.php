f6a16fcedbbd63ed19dfa11280783bc5c8d6c19a
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-03 14:46:54 +0100

    feat: add seeder for database

--- backend/Altavia-Airlines/database/seeders/DatabaseSeeder.php
+++ backend/Altavia-Airlines/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Airplane;
+use App\Models\City;
+use App\Models\Flight;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => 'admin',
+            'role' => 'admin',
         ]);
+
+        City::factory(100)->create();
+
+        Airplane::factory(15)->create();
+
+        Flight::factory(25)->create()->each(function($flight) {
+            $cities = City::all()->random()->limit(2)->pluck('id');
+            $flight->cities()->attach($cities);
+        });
+
+        User::factory(30)->create()->each(function($user) {
+            $flights = Flight::all()->random()->limit(rand(1,10))->pluck('id');
+            $user->flights()->attach($flights);
+        });
     }
 }
