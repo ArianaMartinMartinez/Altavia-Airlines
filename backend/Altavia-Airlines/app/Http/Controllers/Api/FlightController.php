00ab285208deb9faa1679fc5223fe86f0522b492
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-05 15:12:32 +0100

    feat: add index and show methods for Flights

---
+++ backend/Altavia-Airlines/app/Http/Controllers/Api/FlightController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Flight;
+use Illuminate\Http\Request;
+
+class FlightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $flights = Flight::with('airplane', 'cities')->get();
+
+        return response()->json($flights, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $flight = Flight::with('airplane', 'cities')->findOrFail($id);
+
+        return response()->json($flight, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
