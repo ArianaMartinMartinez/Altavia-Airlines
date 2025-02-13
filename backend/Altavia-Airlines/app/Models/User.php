91a959162693b8aa21ac5610a9f9cd48150ab49c
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-13 11:02:57 +0100

    feat: add JWT to auth in api

--- backend/Altavia-Airlines/app/Models/User.php
+++ backend/Altavia-Airlines/app/Models/User.php
@@ -8,8 +8,9 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -51,5 +52,25 @@ class User extends Authenticatable
 
     public function flights(): BelongsToMany {
         return $this->belongsToMany(Flight::class, 'flight_user');
+    }
+
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+ 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
