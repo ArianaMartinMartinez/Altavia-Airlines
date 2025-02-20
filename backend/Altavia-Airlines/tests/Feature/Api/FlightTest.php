7307b43ae6dba6126447a145f961847f7fe2b44a
Ariana <104553851+ArianaMartinMartinez@users.noreply.github.com>
2025-02-20 13:07:04 +0100

    test: test book and cancel flight

--- backend/Altavia-Airlines/tests/Feature/Api/FlightTest.php
+++ backend/Altavia-Airlines/tests/Feature/Api/FlightTest.php
@@ -30,6 +30,30 @@ class FlightTest extends TestCase
         return $response->json('access_token');
     }
 
+    private function authUser() {
+        $user = User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@example.com',
+            'password' => 'user',
+            'role' => 'user',
+        ]);
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'user',
+        ]);
+
+        return $response->json('access_token');
+    }
+
+    private function logout($token) {
+        $response = $this->post(route('logout'), [
+            'token' => $token,
+        ]);
+
+        return $response->json('message');
+    }
+
     public function test_CheckIfCanReceiveAllFlightsWihtApi() {
         Airplane::factory(5)->create();
         Flight::factory(3)->create();
@@ -95,6 +119,26 @@ class FlightTest extends TestCase
         ]);
 
         $data = ['message' => 'Introduced data is not correct'];
+
+        $response->assertStatus(400)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfFlightCreateReturnErrorIfBadRequestWithSameCityWithApi() {
+        $token = $this->authAdmin();
+        Airplane::factory()->create();
+        City::factory(5)->create();
+        
+        $response = $this->post(route('apiStoreFlight'), [
+            'token' => $token,
+            'date' => '2025-02-06',
+            'price' => 200,
+            'airplane_id' => 1,
+            'departure_id' => 2,
+            'arrival_id' => 2,
+        ]);
+
+        $data = ['message' => 'Departure and arrival cities must be different'];
 
         $response->assertStatus(400)
             ->assertJsonFragment($data);
@@ -191,5 +235,110 @@ class FlightTest extends TestCase
         $response = $this->get(route('apiHomeFlights'));
         $response->assertStatus(200)
             ->assertJsonCount(1);
+    }
+
+    public function test_CheckIfCanBookAFlightWithApi() {
+        $token = $this->authUser();
+        Airplane::factory()->create();
+        City::factory(5)->create();
+        Flight::factory()->create();
+
+        $response = $this->post(route('apiBookFlight', 1), [
+            'token' => $token,
+        ]);
+        $data = ['message' => 'Flight booked successfully'];
+
+        $response->assertStatus(200)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfReturnErrorIfBookSameFlightWithApi() {
+        $token = $this->authUser();
+        Airplane::factory()->create();
+        City::factory(5)->create();
+        Flight::factory()->create();
+
+        $response = $this->post(route('apiBookFlight', 1), [
+            'token' => $token,
+        ]);
+        $data = ['message' => 'Flight booked successfully'];
+
+        $response->assertStatus(200)
+            ->assertJsonFragment($data);
+
+        $response = $this->post(route('apiBookFlight', 1), [
+            'token' => $token,
+        ]);
+        $data = ['message' => 'You have already booked this flight'];
+
+        $response->assertStatus(400)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfReturnErrorIfBookFlightWithNoSeatsWithApi() {
+        $tokenAdmin = $this->authAdmin();
+        Airplane::factory()->create([
+            'name' => 'TestAirplane',
+            'seats' => 1
+        ]);
+        City::factory(2)->create();
+        Flight::factory()->create();
+
+        $response = $this->post(route('apiBookFlight', 1), [
+            'token' => $tokenAdmin,
+        ]);
+        $data = ['message' => 'Flight booked successfully'];
+
+        $response->assertStatus(200)
+            ->assertJsonFragment($data);
+        
+        $response = $this->logout($tokenAdmin);
+        $tokenUser = $this->authUser();
+
+        $response = $this->post(route('apiBookFlight', 1), [
+            'token' => $tokenUser,
+        ]);
+        $data = ['message' => 'No seats available for this flight'];
+
+        $response->assertStatus(400)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfCanCancelAFlightWithApi() {
+        $token = $this->authUser();
+        Airplane::factory()->create();
+        City::factory(2)->create();
+        Flight::factory()->create();
+
+        $response = $this->post(route('apiBookFlight', 1), [
+            'token' => $token,
+        ]);
+        $data = ['message' => 'Flight booked successfully'];
+
+        $response->assertStatus(200)
+            ->assertJsonFragment($data);
+
+        $response = $this->post(route('apiCancelFlight', 1), [
+            'token' => $token,
+        ]);
+        $data = ['message' => 'Flight canceled successfully'];
+
+        $response->assertStatus(200)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfReturnErrorIfCancelNotBookedFlightWithApi() {
+        $token = $this->authUser();
+        Airplane::factory()->create();
+        City::factory(2)->create();
+        Flight::factory()->create();
+
+        $response = $this->post(route('apiCancelFlight', 1), [
+            'token' => $token,
+        ]);
+        $data = ['message' => "You haven't booked this flight"];
+
+        $response->assertStatus(400)
+            ->assertJsonFragment($data);
     }
 }
